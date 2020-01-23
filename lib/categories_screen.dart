8faef8354a036c4a9b27d5ef8044931401526d04
Shadow-250 <phung1710246@gmail.com>
2020-01-23 23:00:10 +0700

    Register a Screen as th Main Screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/categories_item.dart';
+
+import './dummy_data.dart';
+import './models/category.dart';
+
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3/2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
       ),
-      children: <Widget>[
-        
-      ],
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      children: DUMMY_CATEGORIES.map((catData) => CategoryItem(catData.title, catData.color)).toList(),
+      ),
     );
   }
 }
