a2923afb168ccb9a0ee39f183dd5b6ca94777035
Shadow-250 <phung1710246@gmail.com>
2020-01-29 00:16:37 +0700

    Bottom Navigator Bar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meal_app/screens/favorites-screen.dart';
 
+import './favorites-screen.dart';
 import './categories_screen.dart';
 
 class TabsSceen extends StatefulWidget {
@@ -9,6 +9,19 @@ class TabsSceen extends StatefulWidget {
 }
 
 class _TabsSceenState extends State<TabsSceen> {
+  final List<Map<String, Object>> _page = [
+    {'page' : CategoriesScreen(), 'title' : 'Categories'},
+    {'page' : FavotitesScreen(), 'title' : 'Favorites'}
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectpage(int index){
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -17,23 +30,28 @@ class _TabsSceenState extends State<TabsSceen> {
       child: Scaffold(
         appBar: AppBar(
           title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favorites',
-              ),
-            ],
-          ),
         ),
-        body: TabBarView(children: <Widget>[
-          CategoriesScreen(),
-          FavotitesScreen(),
-        ],),
+        body: _page[_selectedPageIndex]['page'],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) => _selectpage(index),
+          backgroundColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).accentColor,
+          currentIndex: _selectedPageIndex,
+          type: BottomNavigationBarType.shifting,
+          items: [
+            BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.category),
+              title: Text(_page[_selectedPageIndex]['title']),
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: Theme.of(context).primaryColor,
+              icon: Icon(Icons.favorite),
+              title: Text(_page[_selectedPageIndex]['title']),
+            ),
+          ],
+        ),
       ),
     );
   }
