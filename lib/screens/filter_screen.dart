a18eb1b9eb50f2a9e7681182b7653c15df24817f
Shadow-250 <phung1710246@gmail.com>
2020-01-29 16:46:33 +0700

    I'm fed up commit this change

--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -1,12 +1,98 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
 
-class FilterScreen extends StatelessWidget {
+class FilterScreen extends StatefulWidget {
   static const routeName = '/filters';
+  @override
+  _FilterScreenState createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  var _glutenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
+
+  Widget _buildSwitchListTile(
+    String title, 
+    String description,
+    bool currentValue,
+    Function updateValue,
+  ){
+    return SwitchListTile(
+      title: Text(title),
+      value: _glutenFree,
+      subtitle: Text(
+        description,
+      ),
+      onChanged: updateValue,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Filter'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Filter'),
+      ),
+      // drawer: MainDrawer(),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection.',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _buildSwitchListTile(
+                  'Gluten-free', 
+                  'Only include gluten-free meals', 
+                  _glutenFree,
+                  (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  } 
+                ),
+                _buildSwitchListTile(
+                  'Lactose-free', 
+                  'Only include Lactose-free meals', 
+                  _lactoseFree,
+                  (newValue) {
+                    setState(() {
+                      _lactoseFree = newValue;
+                    });
+                  } 
+                ),
+                _buildSwitchListTile(
+                  'Vegetarian', 
+                  'Only include Vegetarian meals', 
+                  _glutenFree,
+                  (newValue) {
+                    setState(() {
+                      _vegetarian = newValue;
+                    });
+                  } 
+                ),
+                _buildSwitchListTile(
+                  'Vegan', 
+                  'Only include Vegan meals', 
+                  _glutenFree,
+                  (newValue) {
+                    setState(() {
+                      _vegan = newValue;
+                    });
+                  } 
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
     );
   }
 }
--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -3,17 +3,24 @@ import 'package:flutter/material.dart';
 import '../screens/filter_screen.dart';
 
 class MainDrawer extends StatelessWidget {
-  Widget buildListTile(String title, IconData icon, Function tapHandler){
+  Widget buildListTile(String title, IconData icon, Function tapHandler) {
     return ListTile(
-      leading: Icon(icon, size: 26,),
-      title: Text(title, style: TextStyle(
-        fontFamily: 'RobotoCondensed',
-        fontWeight: FontWeight.bold,
-        )
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
       ),
       onTap: tapHandler,
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -28,27 +35,20 @@ class MainDrawer extends StatelessWidget {
             child: Text(
               'Cooking Up!',
               style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 30,
-                color:Theme.of(context).primaryColor,
-              ),
+                  fontWeight: FontWeight.w900,
+                  fontSize: 30,
+                  color: Theme.of(context).primaryColor),
             ),
           ),
-          SizedBox(height: 20,),
-          buildListTile(
-            'Meals', 
-            Icons.restaurant, 
-            (){
-              Navigator.of(context).pushNamed('/');
-            }
+          SizedBox(
+            height: 20,
           ),
-          buildListTile(
-            'Filters', 
-            Icons.settings, 
-            (){
-              Navigator.of(context).pushNamed(FilterScreen.routeName);
-            }
-          ),
+          buildListTile('Meals', Icons.restaurant, () {
+            Navigator.of(context).pushNamed('/');
+          }),
+          buildListTile('Filters', Icons.settings, () {
+            Navigator.of(context).pushNamed(FilterScreen.routeName);
+          }),
         ],
       ),
     );
